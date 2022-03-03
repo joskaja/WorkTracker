7cb1cdc3317e84e19548454967f92c18af176762
Jan Joska <joskaja1@uhk.cz>
2022-03-03 14:26:26 +0100

    Time picker component created

---
+++ frontend/src/components/TimePicker/TimePicker.jsx
@@ -0,0 +1,38 @@
+import { Box } from '@mantine/core'
+import { TimeInput } from '@mantine/dates'
+import moment from 'moment'
+import React, { useState } from 'react'
+import { IoTimeOutline } from 'react-icons/io5'
+import TimeKeeper from 'react-timekeeper'
+
+function TimePicker({time, onChange, label}) {
+    const [showPicker, setShowPicker] = useState(false);
+
+    return (
+        <Box style={{ position: 'relative' }}>
+            <TimeInput
+                label={label}
+                value={moment(time, 'hh:mm').toDate()}
+                onChange={(time) => onChange(moment(time).format('HH:mm'))}
+                icon={<IoTimeOutline />}
+                onFocus={() => setShowPicker(true)}
+                onBlur={() => setShowPicker(false)}
+            />
+            {showPicker &&
+                <Box style={{ position: 'absolute' }}>
+                    <TimeKeeper
+                        hour24Mode
+                        switchToMinuteOnHourSelect
+                        closeOnMinuteSelect
+                        coarseMinutes
+                        time={time}
+                        onChange={time => onChange(time.formatted24)}
+                        onDoneClick={() => setShowPicker(false)}
+                    />
+                </Box>
+            }
+        </Box>
+    )
+}
+
+export default TimePicker
--- frontend/src/components/WorkSessions/WorkSessionForm.jsx
+++ frontend/src/components/WorkSessions/WorkSessionForm.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { Box, Button, Grid, Group, TextInput, Title } from '@mantine/core'
+import React, { useState } from 'react'
+import ClientSelect from '../Clients/ClientSelect'
+import TimePicker from '../TimePicker/TimePicker';
+import { useForm } from '@mantine/hooks'
+
 
 function WorkSessionForm() {
-  return (
-    <div>WorkSessionForm</div>
-  )
+    const form = useForm({
+        initialValues: {
+            startTime: '09:30',
+            endTime: '10:30'
+        }
+    })
+    return (
+        <Box>
+            <Title order={4} mb="sm">Nový záznam</Title>
+            <Grid>
+                <Grid.Col sm={12} md={6} lg={3}>
+                    <ClientSelect />
+                </Grid.Col>
+                <Grid.Col sm={12} md={6} lg={3}>
+                    <ClientSelect />
+                </Grid.Col>
+                <Grid.Col sm={12} md={6} lg={3}>
+                    <Group style={{alignItems: 'center'}} grow>
+                        <TimePicker
+                            label="Začátek"
+                            time={form.values.startTime}
+                            onChange={(time) => form.setFieldValue('startTime', time)}
+                        />
+                        <Box style={{marginTop: 20, width: 20}}> - </Box>
+                        <TimePicker
+                            label="Konec"
+                            time={form.values.endTime}
+                            onChange={(time) => form.setFieldValue('endTime', time)}
+                        />
+                    </Group>
+                </Grid.Col>
+                <Grid.Col sm={12} md={6} lg={3}>
+                    <TextInput
+                        label="Odpracovaná doba"
+                        type="time"
+                    />
+                </Grid.Col>
+                <Grid.Col sm={12}>
+                    <Button>
+                        Uložit záznam
+                    </Button>
+                </Grid.Col>
+            </Grid>
+        </Box>
+    )
 }
 
 export default WorkSessionForm
